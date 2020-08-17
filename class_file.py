db79f9c6b904313bd249ab3a25be1c749e307c9f
lukegtc <lukegtc@aol.com>
2020-08-17 20:31:10 +0200

    gimballing fixed and changing mass and moments fixed

--- class_file.py
+++ class_file.py
@@ -32,79 +32,82 @@ class Planet:
 #height is pulled from a NASA estimate
 
 class Rocket:
-    def __init__(self,cD = 0.3,diameter = 3.7,thrust = np.array([0,0,0]),mass = 1.,
+    def __init__(self,cD = 0.3,diameter = 3.7,thrust = np.array([0,0,0]),mass_empty = 1.,
                  stblzrforce = np.array([0,0,0]),surface_area = 0,height = 40.9,specific_heat = 1000,
-                 payload_cg = np.array([0.,0.,0.]),payload_mass = 0,prop_cg = np.array([0.,0.,0.])):
-        self.fuel = self.Fuel()
+                 payload_cg = np.array([0.,0.,0.]),payload_mass = 0,prop_mass = 0.,tot_cgpos = 0.):
+        self.height = height
+        self.diameter = diameter
+        self.prop_mass = prop_mass
         self.thrust = thrust
-
-        self.mass_empty = mass
+        self.radius = diameter/2
+        self.mass_empty = mass_empty
         self.stblzrforce = stblzrforce
         self.surface_area = surface_area
-        self.height = height
         self.cD = cD
-        self.diameter = diameter
-        self.prop_cg = prop_cg
-        self.prop_mass = self.fuel.mass
+        self.tot_cgpos = tot_cgpos
         self.payload_mass = payload_mass
         self.area = pi*(self.diameter/2)**2
-        self.cgpos_empty = np.array([0.,0.,self.height/2])
+        self.cgpos_empty = np.array([0.,0.,self.height/2])  #add the change in cg due to the payload aka 2nd stage and actual payload
         self.specific_heat = specific_heat #just looked up some similar vals of material similar to the falcon 9 skin
-        self.tot_cgpos_empty = (self.mass_empty*self.cgpos_empty + self.payload_mass*payload_cg)/\
-                               (self.payload_mass+self.mass_empty)
-        self.tot_cgpos = (self.tot_cgpos_empty*(self.mass_empty+self.payload_mass)+self.prop_cg*self.prop_mass)/\
-                         (self.mass_empty+self.payload_mass + self.prop_mass)
-        #around cg at all times (I have no clue if this is correct i sucked at this angular stuff help)
-        self.moi_mat = np.array([[(self.prop_mass+self.mass_empty+self.payload_mass)*(3*(self.diameter/2)**2 +
-                                self.height**2)/12 + (self.mass_empty+self.payload_mass + self.prop_mass)*
-                                  (self.tot_cgpos[1]**2 + self.tot_cgpos[2]**2),0.,0.],
-                  [0.,(self.prop_mass+self.mass_empty+self.payload_mass)*(3*(self.diameter/2)**2 + self.height**2)/12 +
-                   (self.mass_empty+self.payload_mass + self.prop_mass)*(self.tot_cgpos[0]**2 + self.tot_cgpos[2]**2),0.],
-                  [0.,0.,0.5*(self.prop_mass+self.mass_empty+self.payload_mass)*(self.diameter/2)**2 +
-                   (self.mass_empty+self.payload_mass + self.prop_mass)*(self.tot_cgpos[1]**2 + self.tot_cgpos[0]**2)]])
+        self.tot_cgpos_empty = (self.mass_empty*self.cgpos_empty + self.payload_mass*payload_cg)/(self.payload_mass+self.mass_empty)
 
-    def angular_accel_mat(self,torque):
 
-        angular_accel = np.matmul(np.linalg.inv(self.moi_mat),torque)
+    def tot_cgpos1(self,prop_cg):
+        return (self.tot_cgpos_empty * (self.mass_empty + self.payload_mass) + prop_cg * self.prop_mass) /(self.mass_empty + self.payload_mass + self.prop_mass)
+
+    def angular_accel_mat(self,moi_mat,torque):
+
+        angular_accel = np.matmul(np.linalg.inv(moi_mat),torque)
         return angular_accel
 
 
 
 
 
-    class GridFin:
-        def __init__(self, angle, size):
-            self.angle = angle
-            self.size = size
-    #loxtoprop is the ratio of liquid oxygen to the total fuel mass (which is lox + kerosene in the case of the F9)
-    class Propellant:
-        def __init__(self,mass = 0,loxtotot = 0.699,loxtemp = 66,fueltemp = 266.5,radius = 3.7/2):
-            self.mass = mass
+class GridFin:
+    def __init__(self, angle, size):
+        self.angle = angle
+        self.size = size
+#loxtoprop is the ratio of liquid oxygen to the total fuel mass (which is lox + kerosene in the case of the F9)
+class Propellant:
+    def __init__(self,mass = 1.,loxtotot = 0.699,loxtemp = 66,fueltemp = 266.5,radius = 0.,height = 0.,loxvol1 = 0.,fuelvol1 = 0.,tot_prop_cg = 0.):
+        self.mass = mass
+        self.height = height
+        self.loxtemp = loxtemp
+        self.fueltemp = fueltemp
+        self.loxdensity = 1255. #for LOX at 66K
+        self.loxtotot = loxtotot
+        self.loxvol1 = loxvol1
+        self.fuelvol1 = fuelvol1
+        self.fueldensity = 818 #for RP1 at 266.5
+        self.tot_prop_cg = tot_prop_cg
+        self.radius = radius
+     #to find the length of the container for the prop and LOX. This is not the total length but simply part of it
+        self.loxlen = (self.loxvol1-pi*self.radius**3 * 4/3)/(pi*self.radius**2)
+        self.fuellen = (self.fuelvol1-pi*self.radius**3*2/3-(pi*self.radius**3-pi*self.radius**3*2/3))/(pi*self.radius**2)
 
-            self.loxtemp = loxtemp
-            self.fueltemp = fueltemp
-            self.loxdensity = 1255. #for LOX at 66K
-            self.loxmass = loxtotot*mass
-            self.propmass = (1-loxtotot)*mass
-            self.propdensity = 818 #for RP1 at 266.5
-            self.loxvol = self.loxmass/self.loxdensity
-            self.propvol = self.propmass/self.propdensity
-            self.radius = radius
-         #to find the length of the container for the prop and LOX. This is not the total length but simply part of it
-            self.loxlen = (self.loxvol-pi*self.radius**3 * 4/3)/(pi*self.radius**2)
-            self.proplen = (self.propvol-pi*self.radius**3*2/3-(pi*self.radius**3-pi*self.radius**3*2/3))/(pi*self.radius**2)
+    def prop_cg(self,mass1):
+     #   v = pi*h*(3*r**2 - h**2)/3#formula for partial volume of a hemisphere
+        self.mass1 = mass1
+        self.loxmass = self.loxtotot * self.mass1
 
-        def prop_cg(self):
-         #   v = pi*h*(3*r**2 - h**2)/3#formula for partial volume of a hemisphere
-            if self.loxvol> self.loxlen*pi*self.radius**2 + pi*self.radius**3*2/3 and self.loxvol<=self.loxlen*pi*self.radius**2 + pi*self.radius**3*4/3:
+        self.fuelmass = (1 - self.loxtotot) * self.mass1
+
+        self.loxvol = self.loxmass / self.loxdensity
+        self.fuelvol = self.fuelmass / self.fueldensity
+
+        h_remainder = self.height - self.fuellen - self.loxlen - 3 * self.radius
+        if self.mass1 > 0.:
+            if self.loxvol>self.loxlen*pi*self.radius**2 + pi*self.radius**3*2/3 and self.loxvol<=self.loxlen*pi*self.radius**2 + pi*self.radius**3*4/3:
                 loxvol_upper = self.loxvol - self.loxlen*pi*self.radius**2 + pi*self.radius**3*2/3
                 h = self.radius-3*loxvol_upper/(2*pi*self.radius**2)
                 loxupper_mass = loxvol_upper*self.loxdensity
                 zbar_upper = (3*(2*self.radius-h)**2/(4*(3*self.radius-h)))#cg pos if theres still lox in the upper hemisphere of the tank
-                cg_lox_upper = np.array([0.,0.,((self.radius**4*self.loxdensity*pi/4)-(self.loxdensity*2*pi*self.radius**3/
-                                                                                       3-loxupper_mass)*zbar_upper)/loxupper_mass])
+                cg_lox_upper = ((self.radius**4*self.loxdensity*pi/4)-(self.loxdensity*2*pi*self.radius**3/
+                                                                                       3-loxupper_mass)*zbar_upper)/loxupper_mass
 
                 lox_cg = np.array([0.,0., ((cg_lox_upper+self.loxlen+self.radius)*loxupper_mass+(self.loxlen*pi*self.radius**2*self.loxdensity)*(self.loxlen/2 + self.radius) + (self.radius - 3*self.radius/8)*(pi*self.radius**3*2/3*self.loxdensity))/self.loxmass])
+
             elif self.loxvol> pi*self.radius**3*2/3 and self.loxvol<= self.loxlen*pi*self.radius**2 + pi*self.radius**3*2/3:
                 self.loxlen = (self.loxvol - pi * self.radius ** 3 * 2 / 3) / (pi * self.radius ** 2)
 
@@ -117,61 +120,67 @@ class Rocket:
             else:
                 lox_cg = np.array([0.,0.,0.])
 
-            if self.propvol> self.proplen*pi*self.radius**2 + pi*self.radius**3*2/3 and self.propvol <= self.proplen*pi*self.radius**2 + pi*self.radius**3*2/3 + self.radius**3*pi/3:
-                propvol_upper = self.propvol - self.proplen * pi * self.radius ** 2 + pi * self.radius ** 3 * 2 / 3
-                h = self.radius - 3 * propvol_upper / (2 * pi * self.radius ** 2)
-                propupper_mass = propvol_upper * self.propdensity
-                zbar_prop_upper = self.radius-(3 * (2 * self.radius - h) ** 2 / (4 * (
+            if self.fuelvol> self.fuellen*pi*self.radius**2 + pi*self.radius**3*2/3 and self.fuelvol <= self.fuellen*pi*self.radius**2 + pi*self.radius**3*2/3 + self.radius**3*pi/3:
+                fuelvol_upper = self.fuelvol - self.fuellen * pi * self.radius ** 2 + pi * self.radius ** 3 * 2 / 3
+                h = self.radius - 3 * fuelvol_upper / (2 * pi * self.radius ** 2)
+                fuelupper_mass = fuelvol_upper * self.fueldensity
+                zbar_fuel_upper = self.radius-(3 * (2 * self.radius - h) ** 2 / (4 * (
                             3 * self.radius - h)))  # cg pos if theres still prop in the upper portion of the tank
-                cg_prop_upper = np.array([0., 0., ((self.radius ** 4 * self.propdensity * pi / 4) - (
-                            self.loxdensity * 2 * pi * self.radius ** 3 / 3 - propupper_mass) * zbar_prop_upper) / propupper_mass])
+                cg_fuel_upper = ((self.radius ** 4 * self.fueldensity * pi / 4) - (
+                            self.loxdensity * 2 * pi * self.radius ** 3 / 3 - fuelupper_mass) * zbar_fuel_upper) / fuelupper_mass
 
 
-                prop_cg = np.array([0.,0.,((cg_prop_upper + self.proplen + self.radius) * propupper_mass + (
+                fuel_cg = np.array([0.,0.,((cg_fuel_upper + self.fuellen + self.radius) * fuelupper_mass + (
                             self.loxlen * pi * self.radius ** 2 * self.loxdensity) * (self.loxlen / 2 + self.radius) + (
                                       self.radius - 3 * self.radius / 8) * (
-                                      pi * self.radius ** 3 * 2 / 3 * self.propdensity)) / self.propmass])
+                                      pi * self.radius ** 3 * 2 / 3 * self.fueldensity)) / self.fuelmass])
 
-            elif self.propvol>pi*self.radius**3*2/3 and self.propvol<= self.proplen*pi*self.radius**2 + pi*self.radius**3*2/3:
-                self.proplen = (self.propvol - pi * self.radius ** 3 * 2 / 3) / (pi * self.radius ** 2)
+            elif self.fuelvol>pi*self.radius**3*2/3 and self.fuelvol<= self.fuellen*pi*self.radius**2 + pi*self.radius**3*2/3:
+                self.fuellen = (self.fuelvol - pi * self.radius ** 3 * 2 / 3) / (pi * self.radius ** 2)
 
-                prop_cg = np.array([0., 0., ((self.proplen / 2) * (self.propvol - pi * self.radius ** 3 * 2 / 3) * self.propdensity +
-                                             (pi * self.radius ** 3 * 2 / 3) * self.propdensity *
-                                             (self.radius - 3 * self.radius / 8)) / self.propmass])
+                fuel_cg = np.array([0., 0., ((self.fuellen / 2) * (self.fuelvol - pi * self.radius ** 3 * 2 / 3) * self.fueldensity +
+                                             (pi * self.radius ** 3 * 2 / 3) * self.fueldensity *
+                                             (self.radius - 3 * self.radius / 8)) / self.fuelmass])
 
 
 
-            elif self.propvol> 0. and self.propvol<=pi*self.radius**3*2/3:
-                propvol_lower = pi * self.radius ** 3 * 2 / 3
-                h = self.radius - 3 * propvol_lower / (2 * pi * self.radius ** 2)
-                prop_cg = np.array([0., 0., 3 * self.radius / 8 - (3 * (2 * self.radius - h) ** 2 / (4 * (3 * self.radius - h)))])
+            elif self.fuelvol> 0. and self.fuelvol<=pi*self.radius**3*2/3:
+                fuelvol_lower = pi * self.radius ** 3 * 2 / 3
+                h = self.radius - 3 * fuelvol_lower / (2 * pi * self.radius ** 2)
+                fuel_cg = np.array([0., 0., 3 * self.radius / 8 - (3 * (2 * self.radius - h) ** 2 / (4 * (3 * self.radius - h)))])
             else:
-                prop_cg = np.array([0.,0.,0.])
+                fuel_cg = np.array([0.,0.,0.])
+
+
+
+
+            tot_fuel_cg = ((np.array([0.,0.,h_remainder+self.radius+self.fuellen])+lox_cg)*self.fuelmass + (np.array([0.,0.,h_remainder])+fuel_cg)*self.loxmass)/self.mass1
+        else:
+            tot_fuel_cg = np.array([0.,0.,h_remainder])
+
+        return tot_fuel_cg
 
 
 
 
 
+class Engine:
+    def __init__(self,thrust = 0,mdot = 0, pos = 0, isp = 330,gimbal = np.array([0.,0.,0.])):
+        self.thrust = thrust
+        self.isp = isp
+        self.mdot = mdot
+        self.pos = pos
+        self.gimbal = gimbal
 
+    def engine2bodygimbal(self,cgpos,pitch_mat,roll_mat,yaw_mat):
+        new_thrust = np.matmul(yaw_mat,np.matmul(pitch_mat,np.matmul(roll_mat,self.thrust)))
+        print(new_thrust)
+        moments = np.matmul(np.array([[0,new_thrust[2],new_thrust[1]],
+                                      [new_thrust[2],0,new_thrust[0]],
+                                      [new_thrust[1],new_thrust[0],0]]),
+                            abs(cgpos))
 
-
-    class Engine:
-        def __init__(self,thrust = 0,mdot = 0, pos = 0, isp = 330,gimbal = np.array([0.,0.,0.])):
-            self.thrust = thrust
-            self.isp = isp
-            self.mdot = mdot
-            self.pos = pos
-            self.gimbal = gimbal
-
-        def engine2bodygimbal(self,cgpos,pitch_mat,roll_mat,yaw_mat):
-            new_thrust = np.matmul(yaw_mat,np.matmul(pitch_mat,np.matmul(roll_mat,self.thrust)))
-
-            moments = np.matmul(np.array([[0,new_thrust[2],new_thrust[1]],
-                                          [new_thrust[2],0,new_thrust[0]],
-                                          [new_thrust[1],new_thrust[0],0]]),
-                                abs(cgpos))
-
-            return moments
+        return moments
 
 
 class Matrices:
--- rocket_pos_updater.py
+++ rocket_pos_updater.py
@@ -7,14 +7,16 @@ import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 
 #rocket created
-rocket1 = Rocket()
-rocket1.mass_empty = 27200.
+rocket1 = Rocket(prop_mass=418700.,mass_empty=27200.)
+#prop created
+propellant = Propellant(mass=rocket1.prop_mass,radius=rocket1.radius,height=rocket1.height)
+
 #engine Created
-engine1 = rocket1.Engine(mdot=845)
-engine2 = rocket1.Engine(mdot=845)
+engine1 = Engine(mdot=845)
+engine2 = Engine(mdot=845)
 #rocket thrust force
 rocket1.thrust = np.array([0,0,7605e3])
-actual_latitude = 28.5
+actual_latitude = 0
 latitude = (actual_latitude-90)*pi/180
 longitude = 0*pi/180
 #exit velocity
@@ -35,8 +37,8 @@ GM = 3.986004418e14
   # 28 degrees latitude
 earth = Planet(6371e3,g0,layers,avals,base_temp,base_press,r,GM)
 engine1.pos = np.array([0.,0.,0. + earth.radius]) #Changed
-#fuel created
-fuel = rocket1.Propellant(418700)
+
+
 
 
 running =True
@@ -55,6 +57,8 @@ temps = []
 skin_temps = []
 presss = []
 vels = []
+cg_vals = []
+moments= []
 rocket_vel = np.array([0.,0.,0.]) #Dont forget to change some stuff about relative velocity and position ya know
 off_pad = False
 #values for circular orbit
@@ -65,15 +69,16 @@ skin_temp = 0
 angular_vel = np.array([0.,0.,0.])
 total_gimbal = np.array([0.,0.,0.])
 while running:
-    rocket1.prop_mass=fuel.mass
+    tot_prop_cg = propellant.prop_cg(rocket1.prop_mass)
+    propellant.tot_prop_cg = tot_prop_cg
+    rocket1.tot_cgpos = rocket1.tot_cgpos1(tot_prop_cg)
 
-     #estimate from NASA estimated vals
+
+
+
+    #estimate from NASA estimated vals
     radius = sqrt(engine1.pos[0]**2 + engine1.pos[1]**2 + (engine1.pos[2])**2)
-    # theta = np.arctan(sqrt(engine1.pos[0]**2 + engine1.pos[1]**2)/(engine1.pos[2]+earth.radius))
-    # psi = np.arctan(engine1.pos[1]/engine1.pos[0])
-   # cart2sphere = np.array([[sin(theta)*cos(psi),sin(theta)*sin(psi),cos(theta)],
-   #                         [cos(theta)*cos(psi),cos(theta)*sin(psi),-sin(theta)],
-    #                        [-sin(theta),cos(psi),0]])
+
     #calculates pressure, temperature and density using the atmossolver equation from the atmosphere file
     press,temp,density=atmossolver(earth.g0,earth.layers,earth.avals,radius-earth.radius,earth.r,earth.base_temp,earth.base_press)
 
@@ -81,13 +86,13 @@ while running:
     engine1.isp = ve/earth.g0
     #engine1 gimballing
     #Relation of gimballed engine to pitch, roll and yaw angle of rocket
-    # #This is the gimbal of the engine
-    # if fuel.mass<= 0.25*418700:
+    #This is the gimbal of the engine
+    # if rocket1.prop_mass<= 0.25*418700:
     #
     #     engine1.gimbal = np.array([pi/2/1800,0,0])
-
+    #
     # else:
-    #     engine1.gimbal[0] = -pi/2
+    #     engine1.gimbal[0] = 0
     # if engine1.gimbal[2]>= -np.pi/2:
     #     engine1.gimbal-=np.array([0,0,pi/2/1800])
     #
@@ -100,26 +105,56 @@ while running:
 
     #this is the gimbal of the rocket
 
+
+    #moment of inertia
+    moi_mat = np.array([[(rocket1.prop_mass+rocket1.mass_empty+rocket1.payload_mass)*(3*(rocket1.diameter/2)**2 +
+                                rocket1.height**2)/12 + (rocket1.mass_empty+rocket1.payload_mass + rocket1.prop_mass)*
+                                  (rocket1.tot_cgpos[1]**2 + rocket1.tot_cgpos[2]**2),0.,0.],
+                  [0.,(rocket1.prop_mass+rocket1.mass_empty+rocket1.payload_mass)*(3*(rocket1.diameter/2)**2 + rocket1.height**2)/12 +
+                   (rocket1.mass_empty+rocket1.payload_mass + rocket1.prop_mass)*(rocket1.tot_cgpos[0]**2 + rocket1.tot_cgpos[2]**2),0.],
+                  [0.,0.,0.5*(rocket1.prop_mass+rocket1.mass_empty+rocket1.payload_mass)*(rocket1.diameter/2)**2 +
+                   (rocket1.mass_empty+rocket1.payload_mass + rocket1.prop_mass)*(rocket1.tot_cgpos[1]**2 + rocket1.tot_cgpos[0]**2)]])
+
     moment_from_gimbal_engine = engine1.engine2bodygimbal(rocket1.tot_cgpos,pitch_mat,roll_mat,yaw_mat)
-    angular_accel = rocket1.angular_accel_mat(moment_from_gimbal_engine) #CHECK THIS
-  #  print(angular_accel)
+    #print(moment_from_gimbal_engine)
+
+    angular_accel = rocket1.angular_accel_mat(moi_mat,moment_from_gimbal_engine) #CHECK THIS
+
     angular_vel += angular_accel*dt
     total_gimbal +=angular_vel*dt
 
     tot_roll_mat = matrices.roll_matrix(total_gimbal[0])
     tot_pitch_mat = matrices.pitch_matrix(total_gimbal[1])
     tot_yaw_mat = matrices.yaw_matrix(total_gimbal[2])
-    #print(tot_roll_mat,tot_pitch_mat,tot_yaw_mat)
+
     #fuel mass change
-    if fuel.mass <=0:
-        engine1.mdot = 0
-        engine2.mdot = 0
-        fuel.mass = 0
+
 
     thrust1 = engine1.mdot * ve
     thrust1 = np.matmul(yaw_mat,np.matmul(pitch_mat,np.matmul(roll_mat,thrust1)))
     thrust2 = engine2.mdot*ve
-    fuel.mass=fuel.mass - dt*engine1.mdot-dt*engine2.mdot*8
+    rocket1.prop_mass=rocket1.prop_mass - dt*engine1.mdot-dt*engine2.mdot*8
+    if rocket1.prop_mass <=0:
+        engine1.mdot = 0
+        engine2.mdot = 0
+        rocket1.prop_mass = 0
+    #Matrix stuff that was in the classes but now i gotta take it out and into this loop fml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     #Drag force change
     if radius<earth.radius:
@@ -133,20 +168,22 @@ while running:
         #    print(rocket_vel,-np.array([0.,465.1*cos(latitude),465.1*sin(latitude)]))
         else:
             drag_force = np.array([0,0,0])
+
     # if np.linalg.norm(rocket_vel) == 0:
     #     norm_vel = 1
     # else:
     #     norm_vel = np.linalg.norm(rocket_vel)
     #summation of all forces acting on the rocket
+
     rotated_thrust = np.matmul(tot_yaw_mat,np.matmul(tot_pitch_mat,np.matmul(tot_roll_mat,thrust2*8+thrust1)))
     long_yaw_mat = matrices.yaw_matrix(longitude)
     lat_roll_mat = matrices.roll_matrix(latitude)
     lat_pitch_mat = matrices.pitch_matrix(-pi/2)
-    total_force =(rocket1.mass_empty+fuel.mass)*(engine1.pos/radius)*-9.80665+rotated_thrust+drag_force #Fix coordinate stuff
+    total_force =(rocket1.mass_empty+rocket1.prop_mass)*(engine1.pos/radius)*-9.80665+rotated_thrust+drag_force #Fix coordinate stuff
 
 
 
-    rocket_accel = total_force/(rocket1.mass_empty+fuel.mass)
+    rocket_accel = total_force/(rocket1.mass_empty+rocket1.prop_mass)
 
     rocket_vel +=rocket_accel*dt
     engine1.pos +=(np.array([465.1*cos(latitude),465.1*sin(latitude),0.])+rocket_vel)*dt
@@ -157,7 +194,7 @@ while running:
     nose_radius = 0.7 #don't know if this is true but eh close enough for F9
     if radius<= 6371e3 + earth.layers[-1]*10**3:
         #added temp per dt
-        fric_temp = 1.83*10**(-4)*25*np.linalg.norm(rocket_vel)**3*sqrt(density/nose_radius)/rocket1.specific_heat/(rocket1.mass_empty+fuel.mass) #the 25 sqm is the SA of the nose cone exposed to the air friction
+        fric_temp = 1.83*10**(-4)*25*np.linalg.norm(rocket_vel)**3*sqrt(density/nose_radius)/rocket1.specific_heat/(rocket1.mass_empty+rocket1.prop_mass) #the 25 sqm is the SA of the nose cone exposed to the air friction
         skin_temp+=fric_temp
     else:
         if skin_temp >=2:
@@ -173,7 +210,11 @@ while running:
     if t > 7000 or radius<earth.radius:
         running = False
         break
+
+
     plotter_pos = np.matmul(long_yaw_mat,np.matmul(lat_pitch_mat,np.matmul(lat_roll_mat, (engine1.pos)))) #
+
+    #plotting section
     zpos.append(plotter_pos[2])
     ypos.append(plotter_pos[1])
     xpos.append((plotter_pos[0]))
@@ -182,11 +223,13 @@ while running:
     dragforce.append(np.linalg.norm(drag_force))
     time1.append(t)
     time.append(t)
+    moments.append(moment_from_gimbal_engine[0])
     densitys.append(density)
     temps.append(temp)
     presss.append(press)
     rads.append(radius-6371e3)
     vels.append(np.linalg.norm(rocket_vel))
+    cg_vals.append(rocket1.tot_cgpos[2])
 mpl.use('Qt5Agg')
 fig = plt.figure()
 # Make data
@@ -238,9 +281,9 @@ plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
 
-plt.title('Altitude')
-plt.plot(time,rads)
-print(max(rads))
+plt.title('moments')
+plt.plot(time,moments)
+#print(max(rads))
 plt.show()
 plt.title('Drag Force')
 plt.plot(time,dragforce,color = 'r')
